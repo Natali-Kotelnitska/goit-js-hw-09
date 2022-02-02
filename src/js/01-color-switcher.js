8bb5130d2a7ab93f0ac3028f8546496d23c85f5c
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-02-02 12:38:11 +0200

    Update 01-color-switcher.js
    Finish Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtnRef = document.querySelector('button[data-start]');
+const stopBtnRef = document.querySelector('button[data-stop]');
+let timerId = null;
 
+startBtnRef.addEventListener('click', onStarColorSwitcher);
+stopBtnRef.addEventListener('click', onStopColorSwitcher);
+
+function onStarColorSwitcher() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtnRef.setAttribute('disabled', 'disabled');
+}
+
+function onStopColorSwitcher() {
+  clearInterval(timerId);
+  document.body.style.backgroundColor = '';
+  startBtnRef.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
