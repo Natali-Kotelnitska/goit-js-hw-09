8ae1e33cbb92b503adea02ada15be4470c9c2c96
Nataliia Kotelnitska <nkotelnicka@gmail.com>
2022-02-06 23:29:31 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,36 +5,40 @@ formRef.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
-
   const {
     elements: { delay, step, amount },
   } = e.currentTarget;
 
+  const { promisesAmount, firstDelay, delayStep } = getOutput(delay, step, amount);
+
+  getPromises(promisesAmount, firstDelay, delayStep);
+}
+function getOutput(delay, step, amount) {
   let delayStep = Number(step.value);
   let firstDelay = Number(delay.value);
   let promisesAmount = Number(amount.value);
 
-  getPromises(promisesAmount);
+  return { delayStep, firstDelay, promisesAmount };
+}
 
-  function getPromises(amount) {
-    for (let i = 1; i <= amount; i++) {
-      createPromise(i, firstDelay)
-        .then(result => {
-          Notiflix.Notify.success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`, {
-            timeout: 10000,
-            clickToClose: true,
-            useIcon: false,
-          });
-        })
-        .catch(({ position, delay }) => {
-          Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
-            timeout: 10000,
-            clickToClose: true,
-            useIcon: false,
-          });
+function getPromises(amount, firstDelay, delayStep) {
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, firstDelay)
+      .then(result => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${result.position} in ${result.delay}ms`, {
+          timeout: 10000,
+          clickToClose: true,
+          useIcon: false,
         });
-      firstDelay += delayStep;
-    }
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          timeout: 10000,
+          clickToClose: true,
+          useIcon: false,
+        });
+      });
+    firstDelay += delayStep;
   }
 }
 
